a288074623443f305fb01f84549d21a994a4754f
Viking & Robot <vikingandrobot@gmail.com>
2018-05-21 15:58:09 +0200

    Working example of unit testing using in memory database.

--- dev/iot-rest-api/test/api/dao/message/MessageDAO.js
+++ dev/iot-rest-api/test/api/dao/message/MessageDAO.js
@@ -1,9 +1,46 @@
-const should = require('should');
+const MongodbMemoryServer = require('mongodb-memory-server').default;
+require('should');
 const MessageDAO = require('../../../../api/dao/message/MessageDAO');
-const config = require('../../../../config/database.js');
+const Message = require('../../../../api/models/Message');
+const config = require('../../../config/database.js');
+const { MongoClient } = require('mongodb');
 
-describe('MessageDAO', () => {
+
+let mongoServer;
+let messageDAO;
+
+describe('MessageDAO', function describeMessageDAO() {
+  // Here we set the timeout really high to allow to download the libs the first time
+  this.timeout(120000);
+
+  before(async (done) => {
+    mongoServer = new MongodbMemoryServer(config);
+    const port = await mongoServer.getPort();
+    const dbHost = 'localhost';
+    const dbName = await mongoServer.getDbName();
+    messageDAO = new MessageDAO({
+      dbAddress: dbHost,
+      dbPort: port,
+      dbName,
+      mongoClient: MongoClient,
+    });
+    done();
+  });
+
+  after((done) => {
+    mongoServer.stop();
+    done();
+  });
+
   describe('saveOne()', () => {
-
+    this.timeout(200);
+    it('should return a Promise.', (done) => {
+      const p = messageDAO.saveOne(new Message('author', 'message'));
+      p.should.be.a.Promise();
+      p.then(
+        () => { done(); },
+        () => { done(new Error('The Promise was rejected')); }
+      );
+    });
   });
 });
---
+++ dev/iot-rest-api/test/config/database.js
@@ -0,0 +1,7 @@
+const TEST_DATABASE_CONFIG = {
+  instance: {
+    dbName: 'iot_back_end_test',
+  },
+};
+
+module.exports = TEST_DATABASE_CONFIG;
