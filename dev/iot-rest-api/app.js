632f3096130665355bab98f396c56ba16283a5dc
Guillaume Milani <milani.guillaume@gmail.com>
2018-05-23 11:42:41 +0200

    All endpoints are protected by JWT, TODO: blacklist

--- dev/iot-rest-api/app.js
+++ dev/iot-rest-api/app.js
@@ -3,10 +3,42 @@
 require('dotenv').load();
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var jwt = require('jsonwebtoken');
+
 module.exports = app; // for testing
 
+if (process.env.JWT_SECRET === undefined) {
+  console.error("Undefined JWT_SECRET in .env file. It was nice knowing you... Argh...");
+  console.log("Exiting");
+  process.exit(1);
+}
+const JWT_SECRET = process.env.JWT_SECRET;
+
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
+  swaggerSecurityHandlers: {
+    Bearer: function jwtSecurityHandler(req, authOrSecDef, scopesOrApiKey, callback) {
+      var token = null;
+
+      // Read the token from header
+      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, JWT_SECRET, (err, verified) => {
+          if (err) {
+            callback(new Error("Invalid token"));
+            return;
+          } else {
+            // TODO: Check in DB if 
+            callback();
+            return;
+          }
+        });
+      } else {
+        callback(new Error("No auth token found"));
+        return;
+      }      
+    }
+  }
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
