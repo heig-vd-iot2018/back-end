5bfa0a9d05439fd75f44f8cc472794ad0b58d636
Viking & Robot <vikingandrobot@gmail.com>
2018-06-07 18:42:49 +0200

    Unit test findAll.

--- dev/iot-rest-api/test/api/dao/sensor/NodeDAO.js
+++ dev/iot-rest-api/test/api/dao/sensor/NodeDAO.js
@@ -10,6 +10,20 @@ let mongoServer;
 let testDatabaseConfig;
 let nodeDAO;
 
+function createRandomNode() {
+  const node = new Node(
+    uuidv4(),
+    Date.now(),
+    Date.now(),
+    true,
+    'asdf',
+    'jkle',
+    []
+  );
+
+  return node;
+}
+
 describe('SensorDAO', function describeSensorDAO() {
   // Here we set the timeout really high to allow to download the libs the first time
   this.timeout(120000);
@@ -29,6 +43,28 @@ describe('SensorDAO', function describeSensorDAO() {
     };
     // Finished Before
     done();
+  });
+
+  beforeEach((done) => {
+    const { mongoClient } = testDatabaseConfig;
+    const url = `mongodb://${testDatabaseConfig.dbAddress}:${testDatabaseConfig.dbPort}`;
+    mongoClient.connect(url, (err, client) => {
+      if (err !== null) {
+        done(err);
+      } else {
+        const db = client.db(testDatabaseConfig.dbName);
+        const collection = db.collection('nodes');
+        // Insert some documents
+        collection.deleteMany({}, (error) => {
+          if (error !== null) {
+            done(error);
+          } else {
+            done();
+          }
+        });
+        client.close();
+      }
+    });
   });
 
   after((done) => {
@@ -95,16 +131,6 @@ describe('SensorDAO', function describeSensorDAO() {
     });
 
     it('should return null if it cannot find an sensor in the database', (done) => {
-      const node = new Node(
-        uuidv4(),
-        Date.now(),
-        Date.now(),
-        true,
-        '',
-        '',
-        []
-      );
-
       nodeDAO = new NodeDAO(testDatabaseConfig);
       nodeDAO.findById(uuidv4())
         .then((n) => {
@@ -114,6 +140,92 @@ describe('SensorDAO', function describeSensorDAO() {
         .catch((err) => {
           done(err);
         });
+    });
+
+    it('should find a Node existing in the database', (done) => {
+      const node = new Node(
+        uuidv4(),
+        Date.now(),
+        Date.now(),
+        true,
+        'asdf',
+        'jkle',
+        []
+      );
+
+      nodeDAO = new NodeDAO(testDatabaseConfig);
+      nodeDAO.saveOne(node)
+        .then(() => nodeDAO.findById(node.id))
+        .then((foundNode) => {
+          should.equal(foundNode.id, node.id);
+          should.equal(foundNode.createdDate, node.createdDate);
+          should.equal(foundNode.updatedDate, node.updatedDate);
+          should.equal(foundNode.active, node.active);
+          should.equal(foundNode.latitude, node.latitude);
+          should.equal(foundNode.longitude, node.longitude);
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    describe('findAll()', () => {
+      it('should return a Promise', (done) => {
+        nodeDAO = new NodeDAO(testDatabaseConfig);
+        const p = nodeDAO.findAll();
+        p.should.be.a.Promise();
+        p.then(() => {
+          done();
+        });
+      });
+
+      it('should resolve an array of Nodes', (done) => {
+        nodeDAO = new NodeDAO(testDatabaseConfig);
+        nodeDAO.findAll()
+          .then((nodes) => {
+            nodes.should.be.Array();
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          });
+      });
+
+      it('should resolve all existing Nodes', (done) => {
+        const nodes = [];
+
+        nodeDAO = new NodeDAO(testDatabaseConfig);
+        nodeDAO.saveOne(createRandomNode())
+          .then((n) => {
+            nodes.push(n);
+            return nodeDAO.saveOne(createRandomNode());
+          })
+          .then((n) => {
+            nodes.push(n);
+            return nodeDAO.saveOne(createRandomNode());
+          })
+          .then((n) => {
+            nodes.push(n);
+            return nodeDAO.saveOne(createRandomNode());
+          })
+          .then((n) => {
+            nodes.push(n);
+            return nodeDAO.saveOne(createRandomNode());
+          })
+          .then((n) => {
+            nodes.push(n);
+            return nodeDAO.findAll();
+          })
+          .then((foundNodes) => {
+            should.equal(foundNodes.length, nodes.length);
+            foundNodes.map(n => n.id).sort().should.deepEqual(nodes.map(n => n.id).sort());
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          });
+      });
     });
   });
 });
