6648f52ed6445344afda3542ce4c00debf322119
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-06-07 14:24:16 +0200

    Eslint complient

--- dev/iot-rest-api/api/controllers/Sensor.js
+++ dev/iot-rest-api/api/controllers/Sensor.js
@@ -85,80 +85,84 @@ function setData(req, res) {
   const sensorData = req.swagger.params.data.value;
 
   // Check the length of the payload
-  if(sensorData.payload.length % 4 !== 0)
-    res.status(400).json({ message: "Bad payload format (Length is incorrect)" });
+  if (sensorData.payload.length % 4 !== 0) {
+    res.status(400).json({ message: 'Bad payload format (Length is incorrect)' });
+  }
 
   // Get the header in binary (Bits fields)
   const headers = sensorData.payload.substr(0, 4);
   const headersInBinary = parseInt(headers, 16).toString(2);
 
-  let datas = [];
+  const datas = [];
   let dataCount = 1;
-  
+
   // Process all the data
-  for(let i = 0; i < headersInBinary.length; i++) {
-    const bit = parseInt(headersInBinary.charAt(i));
+  for (let i = 0; i < headersInBinary.length; i += 1) {
+    const bit = parseInt(headersInBinary.charAt(i), 10);
 
     // If the field is not active
-    if(bit === 0) continue;  
-    
-    // Get the specific field
-    const bits = sensorData.payload.substr(dataCount * 4, 4);
-    
-    // If the field not exists
-    if(bits === '') res.status(400).json({ message: "Bad payload format (Headers and fields are not matching)" });
-    
-    let header;
-    let value;
+    if (bit !== 0) {
+      // Get the specific field
+      const bits = sensorData.payload.substr(dataCount * 4, 4);
 
-    // Map the bit field
-    switch(i) {
-      case 0:
-        header = "temperature";
-        value = parseInt(bits, 16) / 10;
-        break;
-      case 1:
-        header = "pressure";
-        value = parseInt(bits, 16) / 10;
-        break;
-      case 2:
-        header = "humidity";
-        value = parseInt(bits, 16) / 100;
-        break;
-      case 3:
-        header = "gas resistance";
-        value = parseInt(bits, 16) / 10;
-        break;
-      case 4:
-        header = "light";
-        value = parseInt(bits, 16);
-      case 14:
-        header = "die temperature";
-        value = parseInt(bits, 16) / 10;
-        break;
-      case 15:
-        header = "battery voltage";
-        value = parseInt(bits, 16) / 10;
-        break;
-      default:
-        res.status(400).json({ message: "Bad payload format (Use of non-used field)" });
+      // If the field not exists
+      if (bits === '') {
+        res.status(400).json({ message: 'Bad payload format (Headers and fields are not matching)' });
+      }
+
+      let header;
+      let value;
+
+      // Map the bit field
+      switch (i) {
+        case 0:
+          header = 'temperature';
+          value = parseInt(bits, 16) / 10;
+          break;
+        case 1:
+          header = 'pressure';
+          value = parseInt(bits, 16) / 10;
+          break;
+        case 2:
+          header = 'humidity';
+          value = parseInt(bits, 16) / 100;
+          break;
+        case 3:
+          header = 'gas resistance';
+          value = parseInt(bits, 16) / 10;
+          break;
+        case 4:
+          header = 'light';
+          value = parseInt(bits, 16);
+          break;
+        case 14:
+          header = 'die temperature';
+          value = parseInt(bits, 16) / 10;
+          break;
+        case 15:
+          header = 'battery voltage';
+          value = parseInt(bits, 16) / 10;
+          break;
+        default:
+          res.status(400).json({ message: 'Bad payload format (Use of non-used field)' });
+      }
+
+      dataCount += 1;
+
+      const data = new Data(
+        sensorData.id,
+        new Date(),
+        header,
+        value,
+      );
+
+      datas.push(data);
     }
-
-    dataCount += 1;
-
-    const data = new Data(
-      sensorData.id,
-      new Date(),
-      header,
-      value,
-    );
-    
-    datas.push(data);
   }
-  
+
   const { dataDAO } = database;
 
-  dataDAO.saveAll(datas).then((m) => {
+  dataDAO.saveAll(datas).then(() => {
     res.status(200).send();
   }, (err) => {
     res.status(500).json({ message: err.toString() });
