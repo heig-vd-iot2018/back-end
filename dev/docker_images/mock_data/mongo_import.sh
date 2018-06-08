9842f170d2e3fbf9f11ed2e9db2aca49d66d41a6
Nicolas Rod <ncls.rod@gmail.com>
2018-06-08 23:56:11 +0200

    Dirty fix to prevent mongo_data from querying the db before it's ready to accept data

--- dev/docker_images/mock_data/mongo_import.sh
+++ dev/docker_images/mock_data/mongo_import.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sleep 5
+
 mongoimport --host database:27017 --db iot_back_end --collection nodes --jsonArray --file mock_data/Nodes.json
 mongoimport --host database:27017 --db iot_back_end --collection data --jsonArray --file mock_data/Data.json
 mongoimport --host database:27017 --db iot_back_end --collection sensors --jsonArray --file mock_data/Sensors.json
